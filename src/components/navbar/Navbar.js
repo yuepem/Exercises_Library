fbbdcdbdce3d59051fdd37994b9757416c6a1e8d
x_yue <103266340+CrpeDiem@users.noreply.github.com>
2023-04-20 10:56:07 +0200

    navbar finished

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <div>
+        <li>Home</li>
+        <li>Search</li>
+        <li>Result</li>
+    </div>
+  )
+}
+
+export default Navbar
