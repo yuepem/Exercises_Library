a05b9f2e50d88c88faf65651d431b25ed9f2900f
x_yue <103266340+CrpeDiem@users.noreply.github.com>
2023-04-21 10:00:58 +0200

    download data and work with them

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -3,67 +3,11 @@ import './Search.css';
 
 // import { apiOptions, fetchData } from '../../apiRequest/FetchData';
 
-const bodyPartsDATA = [
-  "back",
-  "cardio",
-  "chest",
-  "lower arms",
-  "lower legs",
-  "neck",
-  "shoulders",
-  "upper arms",
-  "upper legs",
-  "waist"
-]
+import { EXERCISEDATA } from "../Data/exerciseData";
+import { BODYPARTSDATA } from "../Data/bodyPartsData";
 
-const exerciseDATA =[ {
-  bodyPart: 'waist',
-  equipment: 'body weight',
-  gifUrl: 'http://d205bpvrqc9yn1.cloudfront.net/2333.gif',
-  id: '2333',
-  name: 'arm slingers hanging straight legs',
-  target: 'abs'
-},
-{
-  bodyPart: 'upper legs',
-  equipment: 'body weight',
-  gifUrl: 'http://d205bpvrqc9yn1.cloudfront.net/3214.gif',
-  id: '3214',
-  name: 'arms apart circular toe touch (male)',
-  target: 'glutes'
-},
-{
-  bodyPart: 'waist',
-  equipment: 'body weight',
-  gifUrl: 'http://d205bpvrqc9yn1.cloudfront.net/0001.gif',
-  id: '0001',
-  name: '3/4 sit-up',
-  target: 'abs'
-},
-{
-  bodyPart: 'waist',
-  equipment: 'body weight',
-  gifUrl: 'http://d205bpvrqc9yn1.cloudfront.net/0002.gif',
-  id: '0002',
-  name: '45° side bend',
-  target: 'abs'
-},
-{
-  bodyPart: 'waist',
-  equipment: 'body weight',
-  gifUrl: 'http://d205bpvrqc9yn1.cloudfront.net/0003.gif',
-  id: '0003',
-  name: 'air bike',
-  target: 'abs'
-},
-{
-  bodyPart: 'back',
-  equipment: 'leverage machine',
-  gifUrl: 'http://d205bpvrqc9yn1.cloudfront.net/0015.gif',
-  id: '0015',
-  name: 'assisted parallel close grip pull-up',
-  target: 'lats'
-}]
+const bodyPartsDATA = BODYPARTSDATA;
+const exerciseDATA = EXERCISEDATA;
 
 
 function Search( { setExercises, setBodyPart } ) {
@@ -71,36 +15,7 @@ function Search( { setExercises, setBodyPart } ) {
   const [search, setSearch] = useState('');
   // const [bodyParts, setBodyParts] = useState(['all']);
 
-  // const bodyParts_url = 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList';
-  // const exercises_url = `https://exercisedb.p.rapidapi.com/exercises`;
-
-  // useEffect(() => {
-  //   const fetchBodyPartsDB = async () => {
-  //     const bodyPartsDB = await fetchData(bodyParts_url, apiOptions);
-  //     console.log(bodyPartsDB)
-
-  //     setBodyParts(bodyPartsDB);
-  //   }
-
-  //   fetchBodyPartsDB();
-  // }, [])
-
-  // const handleSearch = async () => {
-  //   if (search) {
-  //     const exerciseDB = await fetchData(exercises_url, apiOptions);
-
-  //     const targetExercises = exerciseDB.filter(
-  //       (exercise) => 
-  //         exercise.name.toLocaleLowerCase().includes(search)
-  //         || exercise.target.toLocaleLowerCase().includes(search)
-  //         || exercise.equipment.toLocaleLowerCase().includes(search)
-  //         || exercise.bodyPart.toLocaleLowerCase().includes(search)
-  //         );
-        
-  //     setSearch('');
-  //     setExercises(targetExercises);
-  //   };
-  // }
+  
 
 
   const handleSearch = () => {
@@ -132,7 +47,7 @@ function Search( { setExercises, setBodyPart } ) {
       </div>
       <div className='bodyParts_categories'>
         {bodyPartsDATA.map((item) => (
-          <li key={item} className='bodyParts' onClick={(e) => setBodyPart()} >{item}</li>
+          <li key={item} className='bodyParts' onClick={() => setBodyPart(item)} >{item}</li>
         )
         )}
       </div>
